cf58522b8d3bc1755f49c6ed3f87983ea88c3bd1
Ryan Jamison <ryanjamison26@gmail.com>
2019-06-09 23:28:24 -0400

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,11 +2,10 @@ const router = require("express").Router();
 const usersController = require("../../controllers/usersController");
 
 router.route("/")
-
 .post(usersController.create)
 
-router
-.route("/:id")
-.get(usersController.findById);
+// router
+// .route("/:id")
+// .get(usersController.findById);
 
 module.exports = router;
